cbef765384029bc8ae00d428e7a6cf3db2442279
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-13 16:16:27 -0500

    intel graphic drivers

---
+++ intel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt update && apt upgrade -y
+sudo apt install -y gpg-agent wget
+wget -qO - https://repositories.intel.com/graphics/intel-graphics.key |
+  sudo apt-key add -
+sudo apt-add-repository \
+  'deb [arch=amd64] https://repositories.intel.com/graphics/ubuntu focal main'
+sudo apt update
+sudo apt install \
+  intel-opencl-icd \
+  intel-level-zero-gpu level-zero \
+  intel-media-va-driver-non-free libmfx1
+sudo apt install mesa-utils
+glxinfo -B
+  while true;do
+ 
+
+  read -p "Do you want to reboot now(Y/N)? " QUESTION
+  
+  case "${QUESTION}" in
+    [Yy] ) 
+      echo "Rebooting now..."    
+       sudo reboot  
+      ;;
+
+    * ) 
+      echo "Don't forget to Reboot the system to apply changes!"
+      exit
+      ;;
+  esac
+done
