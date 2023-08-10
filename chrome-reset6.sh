45b4afd794ac833b17221c756e24de70996bb29a
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 14:28:30 -0500

    Update and rename chrome-reset5.sh to chrome-reset6.sh

--- chrome-reset6.sh
+++ chrome-reset6.sh
@@ -3,12 +3,12 @@ cd ~/Downloads
 #===========================PASSWORDS=====================================
 echo -e "\x1b[41;37mATTENTION! PRESS [ENTER] KEY TO OPEN CHROME AND MANUALLY BACKUP YOUR PASSWORDS AND BOOKMARKS.\x1b[K\x1b[0m"
 read -p ""
-google-chrome chrome://password-manager/settings
-google-chrome chrome://bookmarks
+chromium chrome://password-manager/settings
+chromium chrome://bookmarks
 #============================================================================
 echo -e "\x1b[41;37mATTENTION! YOU ARE ABOUT TO LOSE ALL YOUR CHROME DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
 read -p ""
-sudo pkill --oldest chrome
+sudo pkill --oldest chrome -f
 sudo rm -r /opt/google/chrome -f
 sudo apt remove google-chrome-stable -f -y
 sudo apt purge google-chrome-stable -f -y
