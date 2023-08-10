53299f808888437cf7415d3f22fa2f04ab00761e
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 14:22:23 -0500

    Update and rename chrome-reset4.sh to chrome-reset5.sh

--- chrome-reset5.sh
+++ chrome-reset5.sh
@@ -4,33 +4,11 @@ cd ~/Downloads
 echo -e "\x1b[41;37mATTENTION! PRESS [ENTER] KEY TO OPEN CHROME AND MANUALLY BACKUP YOUR PASSWORDS AND BOOKMARKS.\x1b[K\x1b[0m"
 read -p ""
 google-chrome chrome://password-manager/settings
-#===========================BOOKMARKS=====================================
-googlechromebookmarks() {
-  cat ~/.config/google-chrome/Default/Bookmarks | \
-  grep -E '\"name|\"url' | \
-  grep -vE '\"type\"\:' | \
-  awk '{print $1 substr($0,index($0,$2))}' | \
-  sed s'/^\"name\"\:/title \= /' | \
-  sed s'/^\"url\"\:/url \= /' | \
-  sed s'/\,$//' | \
-  awk 'BEGIN { 
-              title="";
-              re=""
-             }
-             { 
-              if ( $1 ~ /^url/) { 
-                re=re""title"  "$0"\n\n";
-              } else if ( $1 ~ /^title/ ) { 
-                title=$0"\n"; 
-              }
-            } 
-       END { 
-        print re;
-       }'
-}
+google-chrome chrome://bookmarks
 #============================================================================
 echo -e "\x1b[41;37mATTENTION! YOU ARE ABOUT TO LOSE ALL YOUR CHROME DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
 read -p ""
+sudo pkill --oldest chrome
 sudo rm -r /opt/google/chrome -f
 sudo apt remove google-chrome-stable -f -y
 sudo apt purge google-chrome-stable -f -y
