0a7a7b73a14eb231c15f14b16d07a29e594dd545
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 12:31:08 -0500

    Update chrome-reset.sh

--- chrome-reset.sh
+++ chrome-reset.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-sudo apt update
 echo -e "\x1b[41;37mATTENTION! THIS SCRIPT WILL RESET CHROME, AND YOU WILL LOSE ALL YOUR DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
 read -p ""
-sudo apt purge google-chrome-stable
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt upgrade -y
+cd ~/Downloads
+#===========================PASSWORDS=====================================
+wget https://github.com/scriptsbu/iso_fix/raw/main/storage/retrieve_password.py
+python3 retrieve_password.py
+#===========================BOOKMARKS=====================================
+googlechromebookmarks() {
+  cat ~/.config/google-chrome/Default/Bookmarks | \
+  grep -E '\"name|\"url' | \
+  grep -vE '\"type\"\:' | \
+  awk '{print $1 substr($0,index($0,$2))}' | \
+  sed s'/^\"name\"\:/title \= /' | \
+  sed s'/^\"url\"\:/url \= /' | \
+  sed s'/\,$//' | \
+  awk 'BEGIN { 
+              title="";
+              re=""
+             }
+             { 
+              if ( $1 ~ /^url/) { 
+                re=re""title"  "$0"\n\n";
+              } else if ( $1 ~ /^title/ ) { 
+                title=$0"\n"; 
+              }
+            } 
+       END { 
+        print re;
+       }'
+}
+#============================================================================
+sudo apt remove google-chrome-stable -f -y
+sudo apt purge google-chrome-stable -f -y
+sudo snap install chromium
 exit
