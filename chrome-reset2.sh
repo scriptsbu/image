e42ea12ca647d7b12bf8d536899f97c24baa6bd2
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 13:57:28 -0500

    Update chrome-reset3.sh

--- chrome-reset2.sh
+++ chrome-reset2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-echo -e "\x1b[41;37mATTENTION! THIS SCRIPT WILL RESET CHROME, AND YOU WILL LOSE ALL YOUR DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
-read -p ""
 cd ~/Downloads
 #===========================PASSWORDS=====================================
-wget https://github.com/scriptsbu/iso_fix/raw/main/storage/retrieve_password.py
-python3 retrieve_password.py
-cat passwords.csv
+echo -e "\x1b[41;37mATTENTION! PRESS [ENTER] KEY TO OPEN CHROME AND MANUALLY BACKUP YOUR PASSWORDS AND BOOKMARKS.\x1b[K\x1b[0m"
+read -p ""
+google-chrome chrome://password-manager/settings
 #===========================BOOKMARKS=====================================
 googlechromebookmarks() {
   cat ~/.config/google-chrome/Default/Bookmarks | \
@@ -31,6 +29,9 @@ googlechromebookmarks() {
        }'
 }
 #============================================================================
+echo -e "\x1b[41;37mATTENTION! YOU ARE ABOUT TO LOSE ALL YOUR CHROME DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
+read -p ""
+sudo rm -r /opt/google/chrome -f
 sudo apt remove google-chrome-stable -f -y
 sudo apt purge google-chrome-stable -f -y
 sudo snap install chromium
