fd9cf96fe28832608e4a1a9f8a11fcd064db7042
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 12:16:57 -0500

    Delete #!/bin directory

--- #!/bin/chrome-reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-echo -e "\x1b[41;37mATTENTION! THIS SCRIPT WILL RESET CHROME, AND YOU WILL LOSE ALL YOUR DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
-read -p ""
-sudo apt purge google-chrome-stable
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt upgrade -y
-exit
