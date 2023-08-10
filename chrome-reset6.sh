a67c26a4e330035ea77c3ac102999b2b3e6517c7
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-10 14:33:10 -0500

    Update chrome-reset6.sh

--- chrome-reset6.sh
+++ chrome-reset6.sh
@@ -2,9 +2,9 @@
 cd ~/Downloads
 #===========================PASSWORDS=====================================
 echo -e "\x1b[41;37mATTENTION! PRESS [ENTER] KEY TO OPEN CHROME AND MANUALLY BACKUP YOUR PASSWORDS AND BOOKMARKS.\x1b[K\x1b[0m"
+echo -e "\x1b[41;37mchromium chrome://password-manager/settings\x1b[K\x1b[0m"
+echo -e "\x1b[41;37mchromium chrome://bookmarks\x1b[K\x1b[0m"
 read -p ""
-chromium chrome://password-manager/settings
-chromium chrome://bookmarks
 #============================================================================
 echo -e "\x1b[41;37mATTENTION! YOU ARE ABOUT TO LOSE ALL YOUR CHROME DATA(PASSWORD, BOOKMARKS,ETC.) - PRESS [ENTER] KEY TO PROCEED.\x1b[K\x1b[0m"
 read -p ""
@@ -13,4 +13,5 @@ sudo rm -r /opt/google/chrome -f
 sudo apt remove google-chrome-stable -f -y
 sudo apt purge google-chrome-stable -f -y
 sudo snap install chromium
+chromium
 exit
