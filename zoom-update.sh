904198751763f660040a7043aa4adfff1fe16115
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-21 15:44:51 -0500

    Update zoom-update.sh
    fixes blur screen freeze

--- zoom-update.sh
+++ zoom-update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt remove zoom
-sudo snap install zoom-client
+cd Downloads
+sudo apt update -y
+sudo pkill zoom -f
+sudo pkill zoom-client -f
+sudo apt remove zoom -f
+sudo snap remove zoom-client -f
+wget https://us05web.zoom.us/client/latest/zoom_amd64.deb
+sudo dpkg -i zoom_amd64.deb -y
+sudo rm -r  zoom_amd64.deb
+dpkg -l | grep zoom
+zoom
