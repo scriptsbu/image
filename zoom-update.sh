fd54f13b98521d2db74dd9d5e7f0e75da9ae9cbb
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-21 15:49:20 -0500

    Update zoom-update.sh

--- zoom-update.sh
+++ zoom-update.sh
@@ -6,7 +6,7 @@ sudo pkill zoom-client -f
 sudo apt remove zoom -f
 sudo snap remove zoom-client -f
 wget https://us05web.zoom.us/client/latest/zoom_amd64.deb
-sudo dpkg -i zoom_amd64.deb -y
+sudo dpkg -i zoom_amd64.deb
 sudo rm -r  zoom_amd64.deb
 dpkg -l | grep zoom
 zoom
