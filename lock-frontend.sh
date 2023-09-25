437386beb285a4457dd986d05e8cf3f74ab172bb
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-09-25 15:59:08 -0500

    Update and rename lock-var-issue.sh to lock-frontend.sh

--- lock-frontend.sh
+++ lock-frontend.sh
@@ -8,11 +8,12 @@ do
         "Option 1-Diagnose")
             cd  /var/lib/dpkg/status &&
             scp -r lock$USER.txt wifi@10.20.240.3:/debug/wifi/lock$USER.txt && 
-            echo -e "\x1b[41;37mLog sent.\x1b[K\x1b[0m"
+            echo -e "\x1b[41;37mLog sent.\x1b[K\x1b[0m" && echo -e "\x1b[41;37mLook for "not-installed, config-files, half-installed, unpacked, half-configured, triggers-awaited, triggers-pending.\x1b[K\x1b[0m"
             read -p ""
             ;;
         "Option 2-W/O Fix")
             sudo dpkg --configure -a && echo -e "\x1b[41;37mAll Done!\x1b[K\x1b[0m"
+            read -p ""
             ;;
         "Quit")
             break
