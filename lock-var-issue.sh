ac99ed4e79d106c8b5ff01d15049478e9f6be28c
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-09-25 15:46:35 -0500

    Create lock-var-issue.sh

---
+++ lock-var-issue.sh
@@ -0,0 +1,23 @@
+!/bin/bash
+echo -e "\x1b[41;37mSelect option 1 to diagnose the issue; otherwise, select option 2 to fix it.\x1b[K\x1b[0m"
+PS3=''
+options=("Option 1-Diagnose" "Option 2-Fix" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1-Diagnose")
+            cd  /var/lib/dpkg/status &&
+            scp -r lock$USER.txt wifi@10.20.240.3:/debug/wifi/lock$USER.txt && 
+            echo -e "\x1b[41;37mLog sent.\x1b[K\x1b[0m"
+            read -p ""
+            ;;
+        "Option 2-W/O Fix")
+            sudo dpkg --configure -a && echo -e "\x1b[41;37mAll Done!\x1b[K\x1b[0m"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+exit
