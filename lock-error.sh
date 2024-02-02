576f29a24ab5d3ab157d037f6fe9d23323d2bf3c
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-02-02 11:02:52 -0600

    Update lock-error.sh

--- lock-error.sh
+++ lock-error.sh
@@ -8,13 +8,14 @@ do
         "Option 1-Diagnose")
             sudo cp  /var/lib/dpkg/status /home/$USER/Desktop
             sudo nautilus /home/$USER/Desktop
-            echo -e "\x1b[41;37mSend log to IT thru Slack.\x1b[K\x1b[0m" && echo -e "\x1b[41;37mLook for: 'not-installed, config-files, half-installed, unpacked, half-configured, triggers-awaited, triggers-pending.'\x1b[K\x1b[0m"
+            echo -e "\x1b[41;37mSend log to IT thru Slack.\x1b[K\x1b[0m" && echo -e "\x1b[41;37mLook for: 'not-installed, config-files, half-installed, unpacked, half-configured, triggers-awaited, triggers-pending. Press ENTER to continue.'\x1b[K\x1b[0m"
             read -p ""
             ;;
         "Option 2-W/O Fix")
             sudo dpkg --configure -a
-            echo -e "\x1b[41;37mAll Done!\x1b[K\x1b[0m"
+            echo -e "\x1b[41;37mLook for the package causing the issue on the next window and erase the whole block. Press ENTER to continue!\x1b[K\x1b[0m"
             read -p ""
+            sudo nano /var/lib/dpkg/status
             ;;
         "Quit")
             break
