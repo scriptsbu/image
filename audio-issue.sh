6e0c563a6ab362e3fd481f8b7b43253c4689dc56
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-02-15 14:44:34 -0600

    Update audio-issue.sh

--- audio-issue.sh
+++ audio-issue.sh
@@ -12,7 +12,4 @@ pactl info
 systemctl --user restart pipewire 
 sudo apt remove ofono -y -f
 sudo apt remove ofono-phonesim -y -f
-systemctl --user unmask pipewire
-systemctl --user --now disable pulseaudio.service pulseaudio.socket
-systemctl --user --now enable pipewire{,-pulse}.{socket,service}
 echo "Done!"
