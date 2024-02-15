a16058ecd324ad048a9b4db1ccb2b729d720d7d5
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-02-15 14:36:04 -0600

    Create audio-issue-pipewire.sh

---
+++ audio-issue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo add-apt-r-epository ppa:pipewire-debian/pipewire-upstream -y -f
+sudo apt update
+sudo apt install pipewire -y -f
+sudo apt install libspa-0.2-bluetooth
+sudo apt install pipewire-audio-client-libraries
+systemctl --user daemon-reload
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user mask pulseaudio
+systemctl --user --now enable pipewire-media-session.service
+pactl info
+systemctl --user restart pipewire 
+sudo apt remove ofono -y -f
+sudo apt remove ofono-phonesim -y -f
+echo "Done!"
