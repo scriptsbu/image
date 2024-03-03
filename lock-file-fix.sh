3b38ccd54cdca1b687f4670fbe4b2e22ae62d997
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-03-03 14:08:54 -0600

    Update lock-file-fix.sh

--- lock-file-fix.sh
+++ lock-file-fix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt clean
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock -y -f
+sudo rm /var/lib/apt/lists/lock -y -f
+sudo rm /var/lib/dpkg/lock-frontend -y -f
+sudo rm /var/cache/apt/archives/lock -y -f
 sudo dpkg --configure -a
 echo "Done!"
